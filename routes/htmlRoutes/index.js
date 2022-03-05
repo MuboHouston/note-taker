e90cf57d30c535a8a06116186317998c129df3bd
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-03-05 13:07:45 -0600

    fix db.json path in htmlRoutes/index.js file

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,7 +3,7 @@ const path = require ('path');
 const router = require('express').Router();
 
 //routes that the front-end can request data from
-const { notes } = require('./db/db.json');
+const { notes } = require('../../db/db.json');
 
 //directs user to homepage where there is no endpoint
 router.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const { notes } = require('./db/db.json');
 
+//middleware/parse incoming string or array data
+app.use(express.urlencoded({ extended: true }))
+//parse incoming JSON data
+app.use(express.json());
+
+app.use('/', htmlRoutes);
+
 function createNewNotes (body, notesArray) {
     const note = body;
     notesArray.push(note)
@@ -21,13 +28,6 @@ function createNewNotes (body, notesArray) {
     //return finished code to post route for response
     return note;
 }
-
-//middleware/parse incoming string or array data
-app.use(express.urlencoded({ extended: true }))
-//parse incoming JSON data
-app.use(express.json());
-
-app.use('/', htmlRoutes);
 
 app.post('/notes', (req, res) => {
     //req.body is where our incoming content will be
