534ccd42021c958cbe9204e98e8b6483ee8d202e
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-03-05 13:15:51 -0600

    add validation to the data

--- server.js
+++ server.js
@@ -29,6 +29,16 @@ function createNewNotes (body, notesArray) {
     return note;
 }
 
+function validateNote(note) {
+    if(!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if(!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
+
 app.post('/notes', (req, res) => {
     //req.body is where our incoming content will be
     console.log(req.body);
@@ -36,9 +46,13 @@ app.post('/notes', (req, res) => {
     //set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
 
+    //if any data in req.body is incorrect, send 400 error back
+    if(!validateNote(req.body)) {
+        res.status(400).send('Please ensure that your note includes a title and text!')
+    } else {
     const note = createNewNotes(req.body, notes)
-
     res.json(note)
+    }
 })
 
 //listening for incoming requests
